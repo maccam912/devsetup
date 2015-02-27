dba30b28f556b3ea6608627c5f75c7b3bb4a3d75
Ubuntu <admin@cnc.localdomain>
2015-02-27 16:17:08 +0000

    add zsh

--- devsetup.pp
+++ devsetup.pp
@@ -1,5 +1,8 @@
-include 'docker'
-
 $devtools = ["tmux", "vim", "curl", "git", "build-essential", "golang", "zsh"]
 
 package { $devtools: ensure => "latest" }
+
+exec {"add_zsh_config":
+    command => "/bin/cd ~ && /usr/bin/wget https://raw.githubusercontent.com/maccam912/devsetup/master/zshrc && /bin/mv zshrc .zshrc",
+    creates => "/home/admin/.zshrc"
+}
