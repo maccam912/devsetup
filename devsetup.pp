1292989bedbc1ca79e1b1312fb5f992f44988c6a
Ubuntu <admin@cnc.localdomain>
2015-02-27 17:32:04 +0000

    install pip with easyinstall

--- devsetup.pp
+++ devsetup.pp
@@ -1,6 +1,10 @@
-$devtools = ["tmux", "vim", "curl", "default-jre", "git", "python-pip", "build-essential", "golang", "zsh", "devscripts"]
+$devtools = ["tmux", "vim", "curl", "default-jre", "git", "python", "build-essential", "golang", "zsh", "devscripts"]
 
 package { $devtools: ensure => "latest" }
+
+exec {"pip":
+    command => "/usr/bin/sudo /usr/bin/easy_install pip"
+}
 
 exec {"add_zsh_config":
     command => "/bin/cd ~ && /usr/bin/wget https://raw.githubusercontent.com/maccam912/devsetup/master/zshrc && /bin/mv zshrc .zshrc",
